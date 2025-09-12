0a32ebc26c6c0ca21ddb698c377ba3bc612ff1e5
lamossta <lamossta@fel.cvut.cz>
2025-09-12 12:01:48 +0200

    graph db

---
+++ src/db/graph_db.py
@@ -0,0 +1,49 @@
+import requests
+from SPARQLWrapper import SPARQLWrapper, JSON
+import csv
+from rdflib.namespace import XSD
+
+
+
+class GraphDb:
+
+    GRAPHDB_URL = "http://localhost:7200/repositories"
+
+    def __init__(self, catalog_name: str):
+        self.repo_name = f"{catalog_name}_repo"
+
+    def query_sparql(self, query: str, file_path: str):
+        repo_url = f"{self.GRAPHDB_URL}/{self.repo_name}"
+
+        response = requests.post(
+            repo_url,
+            data=query,
+            headers={"Content-Type": "application/sparql-query",
+                     "Accept": "application/sparql-results+json"}
+        )
+
+        return response.json()
+    
+    def query_sparql_remote(self, query: str, endpoint: str) -> str:
+        sparql = SPARQLWrapper(endpoint)
+        sparql.setReturnFormat(JSON)
+        sparql.setQuery(query)
+        results = sparql.query().convert()
+
+        return results
+    
+    def push_trig_to_repo(self, file_path: str) -> None:
+        rdf_endpoint = f"{self.GRAPHDB_URL}/{self.repo_name}/statements"
+
+        with open(file_path, "rb") as f:
+            data = f.read()
+
+        # HTTP POST request na GraphDB
+        headers = {
+            "Content-Type": "application/x-trig",
+            'Accept': 'application/json'
+        }
+
+        response = requests.put(rdf_endpoint, headers=headers, data=data)
+
+
